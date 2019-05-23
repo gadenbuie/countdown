512d4eda19883de35fee8528639654d7a3455f0a
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-05-22 22:47:07 -0400

    Add/export darken(), lighten() and use both in choose_dark_or_light()

--- tests/testthat/test-colors.R
+++ tests/testthat/test-colors.R
@@ -14,12 +14,20 @@ test_that("col2rgb()", {
   expect_error(col2rgb("#8097"))
 })
 
-test_that("darken_color()", {
-  expect_equal(darken_color("beige", 0.1), "#DCDCC6")
-  expect_equal(darken_color("#f5f5dc", 0.1), "#DCDCC6")
-  expect_equal(darken_color("beige", 1), "#000000")
-  expect_error(darken_color("red", 2))
-  expect_error(darken_color("red", -1))
+test_that("darken()", {
+  expect_equal(darken("beige", 0.1), "#DCDCC6")
+  expect_equal(darken("#f5f5dc", 0.1), "#DCDCC6")
+  expect_equal(darken("beige", 1), "#000000")
+  expect_error(darken("red", 2))
+  expect_error(darken("red", -1))
+})
+
+test_that("lighten()", {
+  expect_equal(lighten("beige", 0.1), "#F6F6DF")
+  expect_equal(lighten("#f5f5dc", 0.1), "#F6F6DF")
+  expect_equal(lighten("beige", 1), "#FFFFFF")
+  expect_error(lighten("red", 2))
+  expect_error(lighten("red", -1))
 })
 
 test_that("rgb2hex()", {
@@ -28,11 +36,11 @@ test_that("rgb2hex()", {
 })
 
 test_that("choose_dark_or_light()", {
-  expect_equal(choose_dark_or_light("#FFFFFF"), "#000000")
-  expect_equal(choose_dark_or_light("#000000"), "#FFFFFF")
-  expect_equal(choose_dark_or_light("firebrick3"), "#FFFFFF")
-  expect_equal(choose_dark_or_light("#cd2626"), "#FFFFFF")
-  expect_equal(choose_dark_or_light("lightblue1"), "#000000")
-  expect_equal(choose_dark_or_light("#bfefff"), "#000000")
+  expect_equal(choose_dark_or_light("#FFFFFF", black = "#000000"), "#000000")
+  expect_equal(choose_dark_or_light("#000000", white = "#FFFFFF"), "#FFFFFF")
+  expect_equal(choose_dark_or_light("firebrick3", white = "#FFFFFF"), "#FFFFFF")
+  expect_equal(choose_dark_or_light("#cd2626",    white = "#FFFFFF"), "#FFFFFF")
+  expect_equal(choose_dark_or_light("lightblue1", black = "#000000"), "#000000")
+  expect_equal(choose_dark_or_light("#bfefff",    black = "#000000"), "#000000")
   expect_equal(choose_dark_or_light("lightblue1", "firebrick3"), "#CD2626")
 })
