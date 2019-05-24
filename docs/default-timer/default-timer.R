32d3d32f5f97677fea9b4fc51a3cdc3edbe0aea2
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-05-23 23:49:41 -0400

    Document countdown_fullscreen()
    (still needs more documentation)

--- docs/default-timer/default-timer.R
+++ docs/default-timer/default-timer.R
@@ -1,5 +1,11 @@
 options(htmltools.dir.version = FALSE)
+dir.create(here::here("docs/default-timer"), showWarnings = FALSE)
 htmltools::save_html(
   countdown::countdown(0, 15, warn_when = 5, top = 0, left = 0),
   file = file.path(getwd(), "docs/default-timer/index.html")
+)
+dir.create(here::here("docs/fullscreen"), showWarnings = FALSE)
+htmltools::save_html(
+  countdown::countdown_fullscreen(0, 15, warn_when = 5),
+  file = file.path(getwd(), "docs/fullscreen/index.html")
 )
