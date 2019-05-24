db3d9e0f0f63ff72f421834000974697e5e41031
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-05-23 21:58:07 -0400

    Document warn_when

--- docs/default-timer/default-timer.R
+++ docs/default-timer/default-timer.R
@@ -1,5 +1,5 @@
 options(htmltools.dir.version = FALSE)
 htmltools::save_html(
-  countdown::countdown(0, 15, top = 0, left = 0),
-  file = "~/repos/countdown/docs/default-timer/index.html"
+  countdown::countdown(0, 15, warn_when = 5, top = 0, left = 0),
+  file = file.path(getwd(), "docs/default-timer/index.html")
 )
