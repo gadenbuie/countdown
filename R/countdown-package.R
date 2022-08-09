5a7fc7753bcf90952c4303594c9e53a8ec3e9066
Garrick Aden-Buie <garrick@adenbuie.com>
2022-08-09 09:38:54 -0400

    Update description and use importFrom section

---
+++ R/countdown-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom htmltools code
+#' @importFrom htmltools div
+#' @importFrom htmltools HTML
+#' @importFrom htmltools htmlDependency
+#' @importFrom htmltools span
+## usethis namespace: end
+NULL
