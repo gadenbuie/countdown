833b644311c2cba002d99a5ff0b074a58dfe5794
Garrick Aden-Buie <garrick@adenbuie.com>
2022-08-08 21:39:02 -0400

    building docs also runs the example timers script

--- docs/example-timers.R
+++ docs/example-timers.R
@@ -1,11 +1,12 @@
+## DON'T RUN INTERACTIVELY, SCRIPT EXECUTED BY index.Rmd ##
 options(htmltools.dir.version = FALSE)
-dir.create(here::here("docs/default-timer"), showWarnings = FALSE)
+dir.create("default-timer", showWarnings = FALSE)
 htmltools::save_html(
   countdown::countdown(0, 15, warn_when = 5, top = 0, left = 0),
-  file = file.path(getwd(), "docs/default-timer/index.html")
+  file = "default-timer/index.html"
 )
-dir.create(here::here("docs/fullscreen"), showWarnings = FALSE)
+dir.create("fullscreen", showWarnings = FALSE)
 htmltools::save_html(
   countdown::countdown_fullscreen(0, 15, warn_when = 5),
-  file = file.path(getwd(), "docs/fullscreen/index.html")
+  file = "fullscreen/index.html"
 )
