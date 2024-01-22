f2570edcab5001fbe0f430a79d3f6ab8ce7317b5
James J Balamuta <coatless@users.noreply.github.com>
2024-01-22 09:10:54 +0000

    Remove capital R directory :(

--- R/R/colors.R
+++
@@ -1,5 +0,0 @@
-choose_dark_or_light <- function(x, dark = NULL, light = NULL, strength = 0.75) {
-  dark <- dark %||% prismatic::clr_darken(x, shift = strength)
-  light <- light %||% prismatic::clr_lighten(x, shift = strength)
-  prismatic::best_contrast(x, c(dark, light))
-}
--- R/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(countdown)
-
-test_check("countdown")
